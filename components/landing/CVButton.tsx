63aea009b8da16b46f40b8a1d5c8711513467a6b
sinster2003 <sindhurvshabaraya2318@gmail.com>
2024-09-01 12:52:29 +0530

    download-resume-view-option

--- components/landing/CVButton.tsx
+++ components/landing/CVButton.tsx
@@ -1,12 +1,13 @@
 "use client"
 
+import Link from "next/link";
 import { Button } from "../ui/button";
 
 const CVButton = () => {
     return(
         <div className="flex flex-col gap-4 mx-5 my-4 items-center md:items-start">
-            <Button className="bg-primary hover:bg-purple-600 w-40 text-white text-base p-6">View CV</Button>
-            <Button className="bg-white hover:bg-gray-200 w-[200px] text-primary text-base p-6">Download CV</Button>
+            <Link href="https://drive.google.com/file/d/1o4xi591m1RbD0H2dX4QqGRC8-Qi9Sjj_/view?usp=sharing" target="_blank"><Button className="bg-primary hover:bg-purple-600 w-40 text-white text-base p-6">View CV</Button></Link>
+            <Link href="https://drive.google.com/uc?export=download&id=1o4xi591m1RbD0H2dX4QqGRC8-Qi9Sjj_" download><Button className="bg-white hover:bg-gray-200 w-[200px] text-primary text-base p-6">Download CV</Button></Link>
         </div>
     );
 }
